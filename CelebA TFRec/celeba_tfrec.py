fc0bbb35bac50ca314adab99023926001d01b08d
pierclgr <pierpasquale.colagrande@gmail.com>
2020-04-25 21:07:55 +0200

    Add attributes to tfrec partitions

--- CelebA TFRec/celeba_tfrec.py
+++ CelebA TFRec/celeba_tfrec.py
@@ -12,30 +12,25 @@ tf.compat.v1.flags.DEFINE_string("number", "", "Number of files.")
 FLAGS = tf.compat.v1.flags.FLAGS
 
 
-def _int64_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-list_attr = ["5_o_Clock_Shadow", "Arched_Eyebrows", "Attractive", "Bags_Under_Eyes", "Bald", "Bangs", "Big_Lips",
-             "Big_Nose", "Black_Hair", "Blond_Hair", "Blurry", "Brown_Hair", "Bushy_Eyebrows", "Chubby", "Double_Chin",
-             "Eyeglasses", "Goatee", "Gray_Hair", "Heavy_Makeup", "High_Cheekbones", "Male", "Mouth_Slightly_Open",
-             "Mustache", "Narrow_Eyes", "No_Beard", "Oval_Face", "Pale_Skin", "Pointy_Nose", "Receding_Hairline",
-             "Rosy_Cheeks", "Sideburns", "Smiling", "Straight_Hair", "Wavy_Hair", "Wearing_Earrings", "Wearing_Hat",
-             "Wearing_Lipstick", "Wearing_Necklace", "Wearing_Necktie", "Young"]
+def map_to_boolean(n):
+    if n == "1":
+        return "True"
+    else:
+        return "False"
 
 
 def main():
     """Main converter function."""
+    if not os.path.exists('tfrecs'):
+        os.makedirs('tfrecs')
+
     # Celeb A
     with open(FLAGS.partition_fn, "r") as infile:
         img_fn_list = infile.readlines()
 
-    print(img_fn_list)
+    attributes_name = img_fn_list[1].split()
 
+    img_fn_list = img_fn_list[2:]
     img_fn_list = [elem.strip().split() for elem in img_fn_list]
 
     fn_root = FLAGS.fn_root
@@ -44,7 +39,7 @@ def main():
     i = 0
     file_out = "tfrecord_{}.tfrec".format(i)
     print(file_out)
-    writer = tf.io.TFRecordWriter(file_out)
+    writer = tf.io.TFRecordWriter("tfrecs/" + file_out)
 
     for example_idx, img_fn in enumerate(img_fn_list):
         if example_idx % 1000 == 0:
@@ -54,7 +49,7 @@ def main():
             i += 1
             file_out = "tfrecord_{}.tfrec".format(i)
             print(file_out)
-            writer = tf.io.TFRecordWriter("/tfrecs/" + file_out)
+            writer = tf.io.TFRecordWriter("tfrecs/" + file_out)
         img_path = os.path.join(fn_root, img_fn[0])
         img_shape = mpimg.imread(img_path).shape
         filename = os.path.basename(img_path)
@@ -63,7 +58,10 @@ def main():
         with tf.io.gfile.GFile(img_path, 'rb') as fid:
             image_data = fid.read()
 
-        attr = img_fn[1:]
+        image_attributes = list(map(map_to_boolean, img_fn[1:]))
+
+        attributes = dict(zip([bytes(elem, 'utf-8') for elem in attributes_name],
+                              [bytes(elem, 'utf-8') for elem in image_attributes]))
 
         example = tf.train.Example(features=tf.train.Features(feature={
             'filename': tf.train.Feature(bytes_list=tf.train.BytesList(value=[filename.encode('utf-8')])),
@@ -71,6 +69,7 @@ def main():
             'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[img_shape[1]])),
             'depth': tf.train.Feature(int64_list=tf.train.Int64List(value=[img_shape[2]])),
             'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_data])),
+            'attributes': tf.train.Feature(bytes_list=tf.train.BytesList(value=attributes))
         }))
 
         writer.write(example.SerializeToString())
