b26916d255da525ac3e626c4667bb3cf9a8bcd74
Simone Gayed Said <49990979+guyez@users.noreply.github.com>
2020-04-25 16:22:34 +0200

    New tfrec

--- CelebA TFRec/celeba_tfrec.py
+++ CelebA TFRec/celeba_tfrec.py
@@ -3,9 +3,7 @@ from __future__ import print_function
 import os
 import os.path
 import imageio
-import scipy.io
-import scipy.io.wavfile
-import scipy.ndimage
+import matplotlib.image as mpimg
 import tensorflow as tf
 
 
@@ -24,6 +22,8 @@ def _int64_feature(value):
 
 def _bytes_feature(value):
 	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+	
+list_attr = ["5_o_Clock_Shadow", "Arched_Eyebrows", "Attractive", "Bags_Under_Eyes", "Bald", "Bangs", "Big_Lips", "Big_Nose", "Black_Hair", "Blond_Hair", "Blurry", "Brown_Hair", "Bushy_Eyebrows", "Chubby", "Double_Chin", "Eyeglasses", "Goatee", "Gray_Hair", "Heavy_Makeup", "High_Cheekbones", "Male", "Mouth_Slightly_Open", "Mustache", "Narrow_Eyes", "No_Beard", "Oval_Face", "Pale_Skin", "Pointy_Nose", "Receding_Hairline", "Rosy_Cheeks", "Sideburns", "Smiling", "Straight_Hair", "Wavy_Hair", "Wearing_Earrings", "Wearing_Hat", "Wearing_Lipstick", "Wearing_Necklace", "Wearing_Necktie", "Young"]
 
 
 def main():
@@ -32,7 +32,7 @@ def main():
 	with open(FLAGS.partition_fn, "r") as infile:
 		img_fn_list = infile.readlines()
 		
-	img_fn_list = [elem.strip() for elem in img_fn_list]
+	img_fn_list = [elem.strip().split() for elem in img_fn_list]
 	
 	fn_root = FLAGS.fn_root
 	num_examples = len(img_fn_list)
@@ -51,22 +51,27 @@ def main():
 			i += 1
 			file_out = "tfrecord_{}.tfrec".format(i)
 			print(file_out)
-			writer = tf.io.TFRecordWriter(file_out)			
-		image_raw = imageio.imread(os.path.join(fn_root, img_fn))
-		rows = image_raw.shape[0]
-		cols = image_raw.shape[1]
-		depth = image_raw.shape[2]
-		image_raw = image_raw.tostring()
-		example = tf.train.Example(
-			features=tf.train.Features(
-				feature={
-					"height": _int64_feature(rows),
-					"width": _int64_feature(cols),
-					"depth": _int64_feature(depth),
-					"image_raw": _bytes_feature(image_raw)
-				}
-			)
-		)
+			writer = tf.io.TFRecordWriter(file_out)	
+		img_path = os.path.join(fn_root, img_fn[0])
+		img_shape = mpimg.imread(img_path).shape
+		filename = os.path.basename(img_path)
+		
+		# Read image data in terms of bytes
+		with tf.io.gfile.GFile(img_path, 'rb') as fid:
+			image_data = fid.read()
+			
+		attr = img_fn[1:]
+			
+		example = tf.train.Example(features = tf.train.Features(feature = {
+			'filename': tf.train.Feature(bytes_list = tf.train.BytesList(value = [filename.encode('utf-8')])),
+			'height': tf.train.Feature(int64_list = tf.train.Int64List(value = [img_shape[0]])),
+			'width': tf.train.Feature(int64_list = tf.train.Int64List(value = [img_shape[1]])),
+			'depth': tf.train.Feature(int64_list = tf.train.Int64List(value = [img_shape[2]])),
+			'image': tf.train.Feature(bytes_list = tf.train.BytesList(value = [image_data])),
+		}))
+		
+		
+		
 		writer.write(example.SerializeToString())
 	writer.close()
 
